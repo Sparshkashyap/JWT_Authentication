fff79c1995151deeb956dbe468569c63c84c8a2e
sparshkashyap <sparshkashyap655@gmail.com>
2026-03-12 21:28:20 +0530

    Update db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-
 const mongo_url = process.env.MONGO_URI;
-console.log("MongoDB URL:", mongo_url);
+
+if (!mongo_url) {
+    console.error("MONGO_URI is not defined");
+    process.exit(1);
+}
 
 mongoose.connect(mongo_url)
-.then(() =>{
-    console.log("MongoDB Connected ....");
-}).catch((err) =>{
-    console.error("Error connecting to MongoDB:", err);
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.error("MongoDB connection error:", err);
 });
